dc17e4e6fde19f214de36d54225d505230f1c61b
warborn <xxthecreatorxx_live@hotmail.com>
2015-11-23 00:05:55 +0000

    Add association between user and articles and add debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
+  before_save { self.emal = email.downcase }
   validates :title,       presence: true,
                           length: { minimum: 5, maximum: 50 }
   validates :description, presence: true,
                           length: { minimum: 10, maximum: 300 }
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    has_many :articles
     validates :username, presence: true,
                          length: { minimum: 3, maximum: 25 },
                          uniqueness: { case_sensitive: false }
